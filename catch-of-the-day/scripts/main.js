afc04042458500ca582ad070d19de05eff8ab919
Jarret Moses <jarret.moses@gmail.com>
2015-11-17 10:01:51 -0500

    actuall finish 9 and 10

--- catch-of-the-day/scripts/main.js
+++ catch-of-the-day/scripts/main.js
@@ -4,6 +4,7 @@ var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var Navigation = ReactRouter.Navigation;
+var History = ReactRouter.History;
 
 var createBrowserHistory = require('history/lib/createBrowserHistory');
 var h = require('./helpers.js');
@@ -13,6 +14,20 @@ var h = require('./helpers.js');
 */
 
 var App = React.createClass({
+  getInitialState: function() {
+    return {
+      fishes: {},
+      order: {}
+    }
+  },
+  addFish: function(fish) {
+    var timestamp = (new Date()).getTime();
+
+    // update the state object
+    this.state.fishes['fish-' + timestamp] = fish;
+    // set the state
+    this.setState({ fishes: this.state.fishes });
+  },
   render: function() {
 
     return (
@@ -21,13 +36,54 @@ var App = React.createClass({
           <Header tagline="Fresh Seafood Market "/>
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} />
       </div>
     )
   }
 });
 
-/* 
+/*
+  Add Fish Form
+  <AddFishForm />
+*/
+
+var AddFishForm = React.createClass({
+  createFish: function(e) {
+    // 1. prevent Default
+    e.preventDefault();
+
+    // 2. Take Data From Form
+    var fish = {
+      name: this.refs.name.value,
+      price: this.refs.price.value,
+      status: this.refs.status.value,
+      desc: this.refs.desc.value,
+      image: this.refs.image.value
+    };
+
+
+    // 3. Add fish to app state
+    this.props.addFish(fish);
+    this.refs.fishForm.reset();
+  },
+  render: function() {
+    return (
+      <form className="fish-edit" ref="fishForm" onSubmit={this.createFish}>
+        <input type="text" ref="name" placeholder="Fish Name"/>
+        <input type="text"ref="price" placeholder="Fish Price"/>
+        <select ref="status">
+          <option value="available">Fresh!</option>
+          <option value="unavailable">Sold Out!</option>
+        </select>
+        <textarea type="text" ref="desc" placeholder="Desc"></textarea>
+        <input type="text" ref="image" placeholder="URL to Image"></input>
+        <button type="submit">Add Item</button>
+      </form>
+    )
+  }
+});
+
+/*
   Header
 */
 
@@ -35,13 +91,13 @@ var Header = React.createClass({
   render: function() {
 
     return (
-      <header className="top">  
+      <header className="top">
         <h1>
-          Catch 
+          Catch
           <span className="ofThe">
-            <span className="of">of</span> 
+            <span className="of">of</span>
             <span className="the">the</span>
-          </span> 
+          </span>
           Day
         </h1>
         <h3 className="tagline"><span>{this.props.tagline}</span></h3>
@@ -50,7 +106,7 @@ var Header = React.createClass({
   }
 });
 
-/* 
+/*
   Order
 */
 
@@ -63,14 +119,18 @@ var Order = React.createClass({
   }
 });
 
-/* 
+/*
   Inventory
 */
 
 var Inventory = React.createClass({
   render: function() {
     return (
-      <p>Inventory</p>
+      <div>
+        <h2>Inventory</h2>
+        <AddFishForm {...this.props} />
+      </div>
+
     )
   }
 });
@@ -81,9 +141,16 @@ var Inventory = React.createClass({
  */
 
 var StorePicker = React.createClass({
-  render: function() {  
+  mixins : [History],
+  goToStore : function(e) {
+    e.preventDefault();
+    // get the data from the input
+    var storeId = this.refs.storeId.value;
+    this.history.pushState(null, '/store/' + storeId);
+  },
+  render: function() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
         <input type="text" ref="storeId" defaultValue={h.getFunName()}required/>
         <input type="Submit"/>
@@ -92,17 +159,17 @@ var StorePicker = React.createClass({
   }
 });
 
-/* 
+/*
   Not Found
 */
 
 var NotFound = React.createClass({
   render: function() {
-    return <h1>Not Found</h1>
-  }
+    return <h1>Not Found</h1>;
+  },
 });
 
-/* 
+/*
   Routes
 */
 
