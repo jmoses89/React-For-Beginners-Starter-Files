cf73ae5de2b373a6f55bd9394c2d83801e54ee08
Jarret Moses <jarret.moses@gmail.com>
2015-11-19 10:07:24 -0500

    12 done

--- catch-of-the-day/scripts/main.js
+++ catch-of-the-day/scripts/main.js
@@ -34,16 +34,43 @@ var App = React.createClass({
       fishes: sampleFishes,
     });
   },
+  renderFish: function(key) {
+    return <Fish key={key} index={key} details={this.state.fishes[key]}/>
+  },
   render: function() {
 
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market "/>
+          <ul className="list-of-fishes">
+            {Object.keys(this.state.fishes).map(this.renderFish)}
+          </ul>
         </div>
         <Order />
         <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
       </div>
+    )
+  }
+});
+
+/*
+  Fish
+*/
+
+var Fish = React.createClass({
+
+  render: function() {
+    var details = this.props.details;
+    return (
+      <li className="menu-fish">
+        <img src={details.image} alt="Fish Image" />
+        <h3 className="fish-name">
+          {details.name}
+          <span className="price">{h.formatPrice(details.price)}</span>
+        </h3>
+        <p>{details.desc}</p>
+      </li>
     )
   }
 });
