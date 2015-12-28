87464dbbec1e93d726b57cea81a8d0545abe7bb6
Jarret Moses <jarret.moses@gmail.com>
2015-12-28 12:12:59 -0500

    14 done

--- catch-of-the-day/scripts/main.js
+++ catch-of-the-day/scripts/main.js
@@ -159,21 +159,48 @@ var Header = React.createClass({
 */
 
 var Order = React.createClass({
+  renderOrder: function(key) {
+    var fish = this.props.fishes[key];
+    var count = this.props.order[key];
+
+    if (!fish) {
+      return <li key={key}>Sorry, fish no longer available</li>;
+    }
+
+    return (
+      <li key={key}>
+        {count}lbs
+        {fish.name}
+        <span className="price">{h.formatPrice(count * fish.price)}</span>
+      </li>
+    )
+  },
+
   render: function() {
     var orderIds = Object.keys(this.props.order);
     var total = orderIds.reduce((prevTotal, key) => {
-      return prevTotal + this.props.order[key].price;
+      var fish = this.props.fishes[key];
+      var count = this.props.order[key];
+      var isAvailable = fish && fish.status === 'available';
+
+      if (fish && isAvailable) {
+        return prevTotal + count * parseInt(fish.price) || 0;
+      }
     }, 0);
 
     return (
       <div className="order-wrap">
-        <h2 className="order-title"></h2>
-        <ul className="order-list">
-
+        <h2 className="order-title">Your Order</h2>
+        <ul className="order">
+          {orderIds.map(this.renderOrder)}
+          <li className="total">
+            <strong>Total: </strong>
+            {h.formatPrice(total)}
+          </li>
         </ul>
       </div>
-    )
-  }
+    );
+  },
 });
 
 /*
@@ -239,6 +266,6 @@ var routes = (
     <Route path="/store/:storeId" component={App}/>
     <Route path="*" component={NotFound}/>
   </Router>
-)
+);
 
 ReactDOM.render(routes, document.querySelector('#main'));
